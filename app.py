d504e7f4cfabaa7cc9ec11605691c327cc9261e0
Siddharth Jain <sidojain30705@gmail.com>
2026-02-04 13:21:52 +0530

    clean code

--- app.py
+++ app.py
@@ -6,21 +6,21 @@ os.environ["TF_USE_LEGACY_KERAS"] = "1"
 import streamlit as st
 import numpy as np
 import pickle
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
 
 # Load the LSTM Model
 model = load_model('next_word_lstm.h5')
 
 # Laod the tokenizer
-with open('tokenizer.pickle','rb') as handle:
-    tokenizer=pickle.load(handle)
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
 
 # Function to predict the next word
 def predict_next_word(model, tokenizer, text, max_sequence_len):
     token_list = tokenizer.texts_to_sequences([text])[0]
     if len(token_list) >= max_sequence_len:
-        token_list = token_list[-(max_sequence_len-1):]     # Ensure the sequence length matches max_sequence_len-1
+        token_list = token_list[-(max_sequence_len - 1):]
 
     token_list = pad_sequences([token_list], maxlen=(max_sequence_len - 1), padding='pre')
     predicted = model.predict(token_list, verbose=0)
@@ -32,7 +32,7 @@ def predict_next_word(model, tokenizer, text, max_sequence_len):
     return None
 
 # Streamlit App
-st.title("Next Word Prediction With LSTM And Early Stopping")
+st.title("Next Word Prediction With LSTM")
 input_text = st.text_input("Enter the sequence of Words", "To be or not to")
 if st.button("Predict Next Word"):
     max_sequence_len = model.input_shape[1] + 1
